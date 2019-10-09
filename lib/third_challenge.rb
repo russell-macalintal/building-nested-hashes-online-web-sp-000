9b16c27183a89332d8a773c832336c7a0084e3e4
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-09 02:43:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,24 +7,24 @@ def third_challenge
       :matriarch => {name: "Lady Montague", age: "54"
         #your key/value pairs here
         },
-      :hero => {name: "Romeo", age: "15", status: "Alive"
+      :hero => {name: "Romeo", age: "15", status: "alive"
         #your key/value pairs here
         },
       :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
-   },
+   }, 
    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"
+      :patriarch => {name: "Lord Capulet", age: "50" 
         #your key/value pairs here
         },
       :matriarch => {name: "Lady Capulet", age: "51"
         #your key/value pairs here
         },
-      :heroine => {name: "Juliet", age: "15", status: "Alive"
+      :heroine => {name: "Juliet", age: "15", status: "alive"
         #your key/value pairs here
         },
       :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}, {name: "Nurse", age: "44", attribute: "worried"}]
    }
   }
 
-
+  
 end
